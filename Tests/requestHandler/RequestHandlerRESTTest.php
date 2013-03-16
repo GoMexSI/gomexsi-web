2f014eee14a9afa4b29f2e7131919bb3c663cd08
MikeCasavecchia <mike.casavecchia@gmail.com>
2013-03-15 19:52:45 -0500

    REST and Mock done for use case one and two
    FindPreyForPredator and FindPredatorForPrey both are finished for both
    REST and Mock data

--- Tests/requestHandler/RequestHandlerRESTTest.php
+++ Tests/requestHandler/RequestHandlerRESTTest.php
@@ -9,7 +9,6 @@ class RequestHandlerRESTTest extends PHPUnit_Framework_TestCase
 	public function setUp()
     {
     	$this->handler = new RequestHandler();
-
     }
     //Test for Use case One
 	public function testGetTrophicServiceRESTFindPreyForPredator()
@@ -30,7 +29,6 @@ class RequestHandlerRESTTest extends PHPUnit_Framework_TestCase
 			$this->assertEquals($expectedPreyNames[$iterator], $value);
 			$iterator++;
 		}
-
 	}
 	//Test for Use case One
 	public function testCreatJSONResponseMockFindPreyForPredator()
@@ -41,6 +39,41 @@ class RequestHandlerRESTTest extends PHPUnit_Framework_TestCase
 		$this->handler->getTrophicService();
 
 		$jsonTestString = '[{"scientificName":"Zalieutes mcgintyi","subjectInstances":[{"prey":["Foraminifera","Goniadella","Goniada maculata","Teleostei","Crustacea","Animalia","Rhachotropis","Paraonidae","Phyllodoce arenae","Opheliidae","Ophiodromus","Spionidae","Amphipoda","Nematoda","Lumbrineridae","Onuphidae","Anchialina typica","Nemertea","Bathymedon","Sediment","Xanthoidea"]}]}]';
+		// http://jsonlint.com/ will format this for anyone who wants to look at it in a more readable structure 
+
+		$jsonObject = $this->handler->creatJSONResponse();
+		$this->assertEquals($jsonTestString, $jsonObject);
+	}
+	//Test for Use case two
+	public function testGetTrophicServiceRESTFindPredatorForPrey()
+	{
+		$trophicResultString = array();
+		$expectedPredNames = array("Zalieutes mcgintyi", "Syacium gunteri", "Pomatoschistus microps", "Zoarces viviparus", "Symphurus plagiusa", "Prionotus roseus", "Stenotomus caprinus", "Syacium papillosum", "Monolene sessilicauda", "Fundulus similis", "Trichopsetta ventralis", "Coelorinchus caribbaeus", "Bembrops anatirostris", "Bellator militaris", "Pomatoschistus minutus", "Leiostomus xanthurus", "Crangon crangon", "Platichthys flesus", "Pleuronectes platessa", "Paralichthyes albigutta", "Retusa obtusa", "Symphurus civitatus");
+
+    	$RESTURL = array("serviceType" => "REST", "preyName" => "Foraminifera");
+    	$this->handler->parsePOST($RESTURL);
+
+		$trophicService = $this->handler->getTrophicService();
+		$trophicResultString = $trophicService->findPredatorForPrey("Foraminifera");
+		
+		$this->assertEquals(count($expectedPredNames), count($trophicResultString));
+		
+		$iterator = 0;
+		foreach ($trophicResultString as $value) {
+			$this->assertEquals($expectedPredNames[$iterator], $value);
+			$iterator++;
+		}
+
+	}
+	//Test for Use case two
+	public function testCreatJSONResponseMockFindPredatorForPrey()
+	{
+		$RESTURL = array("serviceType" => "REST", "preyName" => "Foraminifera");
+    	$this->handler->parsePOST($RESTURL);
+
+		$this->handler->getTrophicService();
+
+		$jsonTestString = '[{"scientificName":"Foraminifera","subjectInstances":[{"pred":["Zalieutes mcgintyi","Syacium gunteri","Pomatoschistus microps","Zoarces viviparus","Symphurus plagiusa","Prionotus roseus","Stenotomus caprinus","Syacium papillosum","Monolene sessilicauda","Fundulus similis","Trichopsetta ventralis","Coelorinchus caribbaeus","Bembrops anatirostris","Bellator militaris","Pomatoschistus minutus","Leiostomus xanthurus","Crangon crangon","Platichthys flesus","Pleuronectes platessa","Paralichthyes albigutta","Retusa obtusa","Symphurus civitatus"]}]}]';
 		// http://jsonlint.com/ will format this for anyone who wants to look at it in a more readable structure 
 
 		$jsonObject = $this->handler->creatJSONResponse();
--- requestHandler/RequestJSONResponse.php
+++ requestHandler/RequestJSONResponse.php
@@ -5,7 +5,6 @@ class RequestJSONResponse
 	public function convertToJSONObject($phpObject) # tis late, so I am leaving this for now, will change to actully convery an object thursday or friday
 	{
 		return(json_encode($phpObject));
-		//return '[{"scientificName": "Scomberomorus cavalla", "subjectInstances": {"prey": [{"scientificName": "Synalpheus latastei"}, {"scientificName": "Lutjanus jocu"}]}}]';
 	}
 
 	public function populateReturnObject($serviceObject, $searchType, $speciesSubject)
