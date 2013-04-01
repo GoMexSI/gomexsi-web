d289af030982a9046e37ae2e6b56f8af97e3c749
Jorrit Poelen <jhpoelen@gmail.com>
2013-04-01 14:56:23 -0700

    make smaller test methods

--- Tests/requestHandler/RequestParserTest.php
+++ Tests/requestHandler/RequestParserTest.php
@@ -1,95 +1,100 @@
  <?php
 
-require_once 'requestHandler/RequestParser.php';
+ require_once 'requestHandler/RequestParser.php';
 
-class RequestParserTest extends PHPUnit_Framework_TestCase
-{
-	private $requestParse;
-	private $postRequest;
+ class RequestParserTest extends PHPUnit_Framework_TestCase
+ {
+ 	private $requestParse;
+ 	private $postRequest;
 
-	public function setUp()
-    {
-    	$this->requestParse = new RequestParser();
-    	$this->postRequest = array("serviceType" => "mock",
-    						   "findPrey"=>"on",
-    						   "subjectName" => "Scomberomorus cavalla");
-    }
-    public function testDetermineServiceType()
-	{
+ 	public function setUp()
+ 	{
+ 		$this->requestParse = new RequestParser();
+ 		$this->postRequest = array("serviceType" => "mock",
+ 			"findPrey"=>"on",
+ 			"subjectName" => "Scomberomorus cavalla");
+ 	}
+ 	public function testDetermineServiceType()
+ 	{
 		#mock
-		$this->requestParse->parse($this->postRequest);
-		$this->assertEquals('mock', $this->requestParse->getServiceType());
+ 		$this->requestParse->parse($this->postRequest);
+ 		$this->assertEquals('mock', $this->requestParse->getServiceType());
 		#REST
-		$this->postRequest["serviceType"] = "REST";
-		$this->requestParse->parse($this->postRequest);
-		$this->assertEquals('REST', $this->requestParse->getServiceType());
+ 		$this->postRequest["serviceType"] = "REST";
+ 		$this->requestParse->parse($this->postRequest);
+ 		$this->assertEquals('REST', $this->requestParse->getServiceType());
 		#live
-		$this->postRequest["serviceType"] = "live";
-		$this->requestParse->parse($this->postRequest);
-		$this->assertEquals('live', $this->requestParse->getServiceType());
-	}
-    public function testDetermineSearchType()
-    {	
+ 		$this->postRequest["serviceType"] = "live";
+ 		$this->requestParse->parse($this->postRequest);
+ 		$this->assertEquals('live', $this->requestParse->getServiceType());
+ 	}
+ 	public function testDetermineSearchTypeMock()
+ 	{	
     	#mock
-    	$this->postRequest["serviceType"] = "mock";
-		$this->requestParse->parse($this->postRequest);
-		$this->assertEquals('findPreyForPredator', $this->requestParse->getSearchType());
+ 		$this->postRequest["serviceType"] = "mock";
+ 		$this->requestParse->parse($this->postRequest);
+ 		$this->assertEquals('findPreyForPredator', $this->requestParse->getSearchType());
 		#mock
-		unset($this->postRequest["findPrey"]);
-		$this->postRequest["findPredators"] = "on";
-		$this->requestParse->parse($this->postRequest);
-		$this->assertEquals('findPredatorForPrey',  $this->requestParse->getSearchType());
+ 		unset($this->postRequest["findPrey"]);
+ 		$this->postRequest["findPredators"] = "on";
+ 		$this->requestParse->parse($this->postRequest);
+ 		$this->assertEquals('findPredatorForPrey',  $this->requestParse->getSearchType());
 		#mock
-		$this->postRequest["suggestion"] = "Scomb";
-		$this->requestParse->parse($this->postRequest);
-		$this->assertEquals('fuzzySearch',  $this->requestParse->getSearchType());
-		unset($this->postRequest["suggestion"]);
+ 		$this->postRequest["suggestion"] = "Scomb";
+ 		$this->requestParse->parse($this->postRequest);
+ 		$this->assertEquals('fuzzySearch',  $this->requestParse->getSearchType());
+ 		unset($this->postRequest["suggestion"]);
+ 	}
 
-		#REST
-		$this->postRequest["serviceType"] = "REST";
-		$this->postRequest["findPrey"] = "on";
-		unset($this->postRequest["findPredators"]);
-		$this->requestParse->parse($this->postRequest);
-		$this->assertEquals('findPreyForPredator', $this->requestParse->getSearchType());
-		#REST
-		unset($this->postRequest["findPrey"]);
-		$this->postRequest["findPredators"] = "on";
-		$this->requestParse->parse($this->postRequest);
-		$this->assertEquals('findPredatorForPrey',  $this->requestParse->getSearchType());
-		#REST
-		$this->postRequest["suggestion"] = "Scomb";
-		$this->requestParse->parse($this->postRequest);
-		$this->assertEquals('fuzzySearch',  $this->requestParse->getSearchType());
-		unset($this->postRequest["suggestion"]);
+ 	public function testDetermineSearchTypeREST() 
+ 	{
+ 		$this->postRequest["serviceType"] = "REST";
+ 		$this->postRequest["findPrey"] = "on";
+ 		unset($this->postRequest["findPredators"]);
+ 		$this->requestParse->parse($this->postRequest);
+ 		$this->assertEquals('findPreyForPredator', $this->requestParse->getSearchType());
 
-		#live
-		$this->postRequest["serviceType"] = "live";
-		$this->postRequest["findPrey"] = "on";
-		unset($this->postRequest["findPredators"]);
-		$this->requestParse->parse($this->postRequest);
-		$this->assertEquals('findPreyForPredator', $this->requestParse->getSearchType());
-		#live
-		unset($this->postRequest["findPrey"]);
-		$this->postRequest["findPredators"] = "on";
-		$this->requestParse->parse($this->postRequest);
-		$this->assertEquals('findPredatorForPrey',  $this->requestParse->getSearchType());
-		#live
-		$this->postRequest["suggestion"] = "Scomb";
-		$this->requestParse->parse($this->postRequest);
-		$this->assertEquals('fuzzySearch',  $this->requestParse->getSearchType());
-		unset($this->postRequest["suggestion"]);
-    }
+ 		unset($this->postRequest["findPrey"]);
+ 		$this->postRequest["findPredators"] = "on";
+ 		$this->requestParse->parse($this->postRequest);
+ 		$this->assertEquals('findPredatorForPrey',  $this->requestParse->getSearchType());
+
+ 		$this->postRequest["suggestion"] = "Scomb";
+ 		$this->requestParse->parse($this->postRequest);
+ 		$this->assertEquals('fuzzySearch',  $this->requestParse->getSearchType());
+ 		unset($this->postRequest["suggestion"]);
+ 	}
+
+ 	public function testDetermineSearchTypeLive() 
+ 	{
+		
+ 		$this->postRequest["serviceType"] = "live";
+ 		$this->postRequest["findPrey"] = "on";
+ 		unset($this->postRequest["findPredators"]);
+ 		$this->requestParse->parse($this->postRequest);
+ 		$this->assertEquals('findPreyForPredator', $this->requestParse->getSearchType());
+
+ 		unset($this->postRequest["findPrey"]);
+ 		$this->postRequest["findPredators"] = "on";
+ 		$this->requestParse->parse($this->postRequest);
+ 		$this->assertEquals('findPredatorForPrey',  $this->requestParse->getSearchType());
+
+ 		$this->postRequest["suggestion"] = "Scomb";
+ 		$this->requestParse->parse($this->postRequest);
+ 		$this->assertEquals('fuzzySearch',  $this->requestParse->getSearchType());
+ 		unset($this->postRequest["suggestion"]);
+ 	}
 
     /**
      * @expectedException CorruptSearchTypeParameterException
      */
-	public function testCorruptSearchTypeParameters()
-	{
-		unset($this->postRequest["findPrey"]);
-		unset($this->postRequest["findPredators"]);
-		$this->requestParse->parse($this->postRequest);
-		$this->requestParse->getSearchType();
-	}
-	
+    public function testCorruptSearchTypeParameters()
+    {
+    	unset($this->postRequest["findPrey"]);
+    	unset($this->postRequest["findPredators"]);
+    	$this->requestParse->parse($this->postRequest);
+    	$this->requestParse->getSearchType();
+    }
+
 }
 ?>
