910049f9c68fc6a22caa97eea9505d538aa7ed5c
Jorrit Poelen <jhpoelen@gmail.com>
2016-03-24 14:10:49 -0700

    refactor / fix test error

--- service/TrophicServiceMock.php
+++ service/TrophicServiceMock.php
@@ -2,6 +2,22 @@
 
 class TrophicServiceMock implements TrophicService 
 {
+
+    private function arrayToContainer($stringArray) {
+        $container = array(); // [row][colum]
+
+        $k=0;
+        #if you change the above array contents, be sure to adjust values below..
+        for($i=0; $i<6; $i++) { // 6 rows of data(mock)
+            $container[$i] = array(); 
+            for($j=0; $j<8; $j++) { // 8 columns
+                $container[$i][$j] = $stringArray[$k];
+                $k+=1;
+            }
+        }
+        return $container;
+    }
+
     public function findPreyForPredator($predatorScientificName) 
     {
         return array('Synalpheus latastei', 'Lutjanus jocu');
@@ -19,87 +35,45 @@ class TrophicServiceMock implements TrophicService
     // should be phased out, after new interaction functionality is added
     public function findObservedPreyForPredator($predatorTaxon, $preyTaxon, $locationConstraints, $mimeType)
     {
-        $stringArray = array("Micropogonias undulatus",  28.645202, -96.099923, 0.0, "Senol Akin", 923695200000, 1,
-                             "Brevoortia patronus",      28.645202, -96.099923, 0.0, "Senol Akin", 923695200000, 3,
-                             "Farfantepenaeus aztecus",  28.645202, -96.099923, 0.0, "Senol Akin", 923695200000, 2,
-                             "Mollusca",                 8.645202,  -96.099923, 0.0, "Senol Akin", 923695200000, 1,
-                             "T-Rex",                    8.645202,  -96.099923, 0.0, "MichaelCas", 923695200000, 3,
-                             "Pterodactyl",              8.645202,  -96.099923, 0.0, "MichaelCas", 923695200000, 3);
-        $container = array(); // [row][colum]
-
-        $k=0;
-        #if you change the above array contents, be sure to adjust values below..
-        for($i=0; $i<6; $i++) { // 6 rows of data(mock)
-            $container[$i] = array(); 
-            for($j=0; $j<7; $j++) { // 7 columns
-                $container[$i][$j] = $stringArray[$k];
-                $k+=1;
-            }
-        }
-        return $container;
+        $stringArray = array("Micropogonias undulatus",  28.645202, -96.099923, 0.0, "Senol Akin", 923695200000, 1,"some WKT",
+                             "Brevoortia patronus",      28.645202, -96.099923, 0.0, "Senol Akin", 923695200000, 3,"some WKT",
+                             "Farfantepenaeus aztecus",  28.645202, -96.099923, 0.0, "Senol Akin", 923695200000, 2,"some WKT",
+                             "Mollusca",                 8.645202,  -96.099923, 0.0, "Senol Akin", 923695200000, 1,"some WKT",
+                             "T-Rex",                    8.645202,  -96.099923, 0.0, "MichaelCas", 923695200000, 3,"some WKT",
+                             "Pterodactyl",              8.645202,  -96.099923, 0.0, "MichaelCas", 923695200000, 3, "some WKT");
+        return $this->arrayToContainer($stringArray);
     }
     // should be phased out, after new interaction functionality is added
     public function findObservedPredatorsForPrey($predatorTaxon, $preyTaxon, $locationConstraints, $mimeType)
     {
-        $stringArray = array("Micropogonias undulatus", 28.645202, -96.099923, 0.0, "Rogers 1977", 923695200000, 1,
-                             "Actinopterygii",          28.645202, -96.099923, 0.0, "Senol Akin",  923695200000, 1,
-                             "Great White",             28.645202, -96.099923, 0.0, "John Mayer",  923695200000, 4,
-                             "Mollusca",                8.645202,  -96.099923, 0.0, "Senol Akin",  923695200000, 4,
-                             "Velociraptor",            8.645202,  -96.099923, 0.0, "MichaelCas",  923695200000, 2,
-                             "Tiger Shark",             8.645202,  -96.099923, 0.0, "MichaelCas",  923695200000, 4);
-        $container = array(); // [row][colum]
-
-        $k=0;
-        #if you change the above array contents, be sure to adjust values below..
-        for($i=0; $i<6; $i++) { // 6 rows of data(mock)
-            $container[$i] = array(); 
-            for($j=0; $j<7; $j++) { // 7 columns
-                $container[$i][$j] = $stringArray[$k];
-                $k+=1;
-            }
-        }
-        return $container;
+        $stringArray = array("Micropogonias undulatus", 28.645202, -96.099923, 0.0, "Rogers 1977", 923695200000, 1,"some WKT",
+                             "Actinopterygii",          28.645202, -96.099923, 0.0, "Senol Akin",  923695200000, 1,"some WKT",
+                             "Great White",             28.645202, -96.099923, 0.0, "John Mayer",  923695200000, 4,"some WKT",
+                             "Mollusca",                8.645202,  -96.099923, 0.0, "Senol Akin",  923695200000, 4,"some WKT",
+                             "Velociraptor",            8.645202,  -96.099923, 0.0, "MichaelCas",  923695200000, 2,"some WKT",
+                             "Tiger Shark",             8.645202,  -96.099923, 0.0, "MichaelCas",  923695200000, 4, "some WKT");
+        return $this->arrayToContainer($stringArray);
     }
+
+
     public function findObservedTargetForSource($predatorTaxon, $preyTaxon, $locationConstraints, $mimeType, $interaction)
     {
         if($interaction->getInteractionTitle() == 'eatenBy') {
-            $stringArray = array("Micropogonias undulatus", 28.645202, -96.099923, 0.0, "Rogers 1977", 923695200000, 1,
-                                 "Actinopterygii",          28.645202, -96.099923, 0.0, "Senol Akin",  923695200000, 1,
-                                 "Great White",             28.645202, -96.099923, 0.0, "John Mayer",  923695200000, 4,
-                                 "Mollusca",                8.645202,  -96.099923, 0.0, "Senol Akin",  923695200000, 4,
-                                 "Velociraptor",            8.645202,  -96.099923, 0.0, "MichaelCas",  923695200000, 2,
-                                 "Tiger Shark",             8.645202,  -96.099923, 0.0, "MichaelCas",  923695200000, 4);
-            $container = array(); // [row][colum]
-
-            $k=0;
-            #if you change the above array contents, be sure to adjust values below..
-            for($i=0; $i<6; $i++) { // 6 rows of data(mock)
-                $container[$i] = array(); 
-                for($j=0; $j<7; $j++) { // 7 columns
-                    $container[$i][$j] = $stringArray[$k];
-                    $k+=1;
-                }
-            }
-            return $container;
+            $stringArray = array("Micropogonias undulatus", 28.645202, -96.099923, 0.0, "Rogers 1977", 923695200000, 1, "some WKT",
+                                 "Actinopterygii",          28.645202, -96.099923, 0.0, "Senol Akin",  923695200000, 1,"some WKT",
+                                 "Great White",             28.645202, -96.099923, 0.0, "John Mayer",  923695200000, 4,"some WKT",
+                                 "Mollusca",                8.645202,  -96.099923, 0.0, "Senol Akin",  923695200000, 4,"some WKT",
+                                 "Velociraptor",            8.645202,  -96.099923, 0.0, "MichaelCas",  923695200000, 2,"some WKT",
+                                 "Tiger Shark",             8.645202,  -96.099923, 0.0, "MichaelCas",  923695200000, 4, "some WKT");
+            return $this->arrayToContainer(stringArray);
         }else {
-            $stringArray = array("Micropogonias undulatus",  28.645202, -96.099923, 0.0, "Senol Akin", 923695200000, 1,
-                                 "Brevoortia patronus",      28.645202, -96.099923, 0.0, "Senol Akin", 923695200000, 3,
-                                 "Farfantepenaeus aztecus",  28.645202, -96.099923, 0.0, "Senol Akin", 923695200000, 2,
-                                 "Mollusca",                 8.645202,  -96.099923, 0.0, "Senol Akin", 923695200000, 1,
-                                 "T-Rex",                    8.645202,  -96.099923, 0.0, "MichaelCas", 923695200000, 3,
-                                 "Pterodactyl",              8.645202,  -96.099923, 0.0, "MichaelCas", 923695200000, 3);
-            $container = array(); // [row][colum]
-
-            $k=0;
-            #if you change the above array contents, be sure to adjust values below..
-            for($i=0; $i<6; $i++) { // 6 rows of data(mock)
-                $container[$i] = array(); 
-                for($j=0; $j<7; $j++) { // 7 columns
-                    $container[$i][$j] = $stringArray[$k];
-                    $k+=1;
-                }
-            }
-            return $container;
+            $stringArray = array("Micropogonias undulatus",  28.645202, -96.099923, 0.0, "Senol Akin", 923695200000, 1,"some WKT",
+                                 "Brevoortia patronus",      28.645202, -96.099923, 0.0, "Senol Akin", 923695200000, 3,"some WKT",
+                                 "Farfantepenaeus aztecus",  28.645202, -96.099923, 0.0, "Senol Akin", 923695200000, 2,"some WKT",
+                                 "Mollusca",                 8.645202,  -96.099923, 0.0, "Senol Akin", 923695200000, 1,"some WKT",
+                                 "T-Rex",                    8.645202,  -96.099923, 0.0, "MichaelCas", 923695200000, 3,"some WKT",
+                                 "Pterodactyl",              8.645202,  -96.099923, 0.0, "MichaelCas", 923695200000, 3,"some WKT");
+            return $this->arrayToContainer($stringArray);
         }
     }
     public function findExternalTaxonURL($taxonName)
