228f42b106ca9d85ab5cf6988b6f63bf24c3f18e
jhpoelen <jhpoelen@xs4all.nl>
2017-11-02 06:35:12 -0700

    add some comments for clarification

--- backup.sh
+++ backup.sh
@@ -1,3 +1,8 @@
+# example backup scripts for wordpress instance hosted at gomexsi.tamucc.edu . 
 export TIMESTAMP=`date +%Y%m%d`
-zip -r gomexsi-html-${TIMESTAMP}.zip html
+
+# dump database
 mysqldump --add-drop-table -h owl.tamucc.edu -u CCS_gomexsi -p CCS_gomexsi | gzip > gomexsi-mysqldump-${TIMESTAMP}.sql.gz
+
+# archive wp files
+zip -r gomexsi-html-${TIMESTAMP}.zip html
